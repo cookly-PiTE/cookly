6e8ac58acbf3f76d268afa4ac8281c0dc3f8e479
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2020-11-28 01:05:13 +0100

    added Recipe model and Ingredient model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from enumchoicefield import ChoiceEnum, EnumChoiceField
 
 # Create your models here.
+
+
+class DifficultyTypes(ChoiceEnum):
+    pass
+
+
+class MealCost(ChoiceEnum):
+    pass
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=300, null=False, default=None)
+    description = models.TextField(null=False, default=None)
+    course_type = models.CharField(max_length=150, null=False, default=None)
+    difficulty = models.CharField(max_length=100)
+    cooking_method = models.CharField(max_length=150, null=False, default=None)
+    cost = models.CharField(max_length=100)
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="ingredients"
+    )
+    name = models.TextField()
