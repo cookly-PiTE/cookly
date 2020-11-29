d794ac6941b9702be3546c7e4739689da8bb84ed
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2020-11-29 23:13:48 +0100

    Addev basic recipe views for testing purposes

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Recipe
 
 
-def index(request):
-    return HttpResponse("Hello, recipes index")
+class RecipeList(ListView):
+    model = Recipe
+    paginate_by = 40
+    template_name = "recipes/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipes'] = Recipe.objects.all()
+        return context
