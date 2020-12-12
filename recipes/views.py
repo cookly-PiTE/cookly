b386d145819d3648a08cd54ed73ea273913847c7
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2020-12-12 19:42:52 +0100

    added simple filtering

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 
@@ -14,3 +15,16 @@ class RecipeListView(ListView):
 class RecipeDetailView(DetailView):
     model = Recipe
     template_name = "recipes/recipe_detail.html"
+
+
+class SearchResultsListView(ListView):
+    model = Recipe
+    paginate_by = 20
+    context_object_name = "recipe_list"
+    template_name = "recipes/search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Recipe.objects.filter(
+            Q(name__icontains=query)
+        )
